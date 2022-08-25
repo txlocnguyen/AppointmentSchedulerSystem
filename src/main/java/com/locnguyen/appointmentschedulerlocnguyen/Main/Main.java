ab89f268f24ad97bc7d72cb85c0ae4a492b4622a
Loc Nguyen <txlocnguyen@gmail.com>
2022-08-24 20:42:23 -0500

    edit main.java

--- src/main/java/com/locnguyen/appointmentschedulerlocnguyen/Main/Main.java
+++ src/main/java/com/locnguyen/appointmentschedulerlocnguyen/Main/Main.java
@@ -1,4 +1,24 @@
 package com.locnguyen.appointmentschedulerlocnguyen.Main;
 
-public class Main {
+import com.locnguyen.appointmentschedulerlocnguyen.helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/loginScreen.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Login Screen");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
 }
